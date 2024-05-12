04e58e6d8114ac852831cf2b99832cc033d9d117
Abrar Habib <abrarhabib285@gmail.com>
2024-05-12 18:35:19 -0400

    Edit: Added some endpoints

--- server/app/api/endpoints.py
+++ server/app/api/endpoints.py
@@ -3,7 +3,11 @@ from flask import Blueprint, request, jsonify
 import bson
 import json
 from app.db import (
+    addHeatMap,
+    removeHeatMap,
     test_db_connection,
+    addUser,
+    authenticate
 )
 
 from flask_cors import CORS
@@ -14,6 +18,72 @@ api_v1 = Blueprint("api_v1", "api_v1", url_prefix="/api/v1")
 
 CORS(api_v1)
 
-@api_v1.route("/test_db_connection", methods=["GET"])
+@api_v1.route("/ping", methods=["GET"])
 def test_db():
-    return test_db_connection()
+    return jsonify(test_db_connection())
+
+@api_v1.route("/addUser", methods=["POST"])
+def add_user():
+    if not request.is_json:
+        return jsonify({"msg": "Not JSON request."}), 400
+    userData = request.get_json()
+    try:
+        result = addUser(userData)
+        if isinstance(result, dict) and "error" in result:
+            return jsonify({"error": "User already exists."}), 400
+        return jsonify({"msg": "User added successfully."}), 200
+    except Exception as e:
+        return jsonify({"msg": str(e)}), 400
+    
+@api_v1.route("/authenticate", methods=["POST"])
+def authenticate_user():
+    if not request.is_json:
+        return jsonify({"msg": "Not JSON request."}), 400
+    userData = request.get_json()
+    try:
+        result = authenticate(userData)
+        match(result):
+            case 0:
+                return jsonify({"msg": "User authenticated."}), 200
+            case 1:
+                return jsonify({"error": "Incorrect password."}), 400
+            case 2:
+                return jsonify({"error": "User not found."}), 400
+    except Exception as e:
+        return jsonify({"msg": str(e)}), 400
+    
+@api_v1.route("/addHeatMap", methods=["POST"])
+def add_map():
+    if not request.is_json:
+        return jsonify({"msg": "Not JSON request."}), 400
+    req = request.get_json()
+    try:
+        # Add the map to the database
+        addHeatMap(req)
+        return jsonify({"msg": "Habits updated successfully."}), 200
+    except Exception as e:
+        return jsonify({"msg": str(e)}), 400
+
+@api_v1.route("/removeHeatMap", methods=["GET"])
+def remove_map():
+    if not request.is_json:
+        return jsonify({"msg": "Not JSON request."}), 400
+    req = request.get_json()
+    try:
+        # Remove the map from the database
+        removeHeatMap(req)
+        return jsonify({"msg": "Habit removed successfully."}), 200
+    except Exception as e:
+        return jsonify({"msg": str(e)}), 400
+    
+@api_v1.route("/getHabits", methods=["GET"])
+def get_habits():
+    if not request.is_json:
+        return jsonify({"msg": "Not JSON request."}), 400
+    req = request.get_json()
+    try:
+        # Get the habits from the database
+        getHabits(req)
+        return jsonify({"msg": "Habits retrieved successfully."}), 200
+    except Exception as e:
+        return jsonify({"msg": str(e)}), 400
--- server/app/db.py
+++ server/app/db.py
@@ -17,16 +17,74 @@ config = configparser.ConfigParser()
 config.read(".ini")
 
 client = MongoClient(config["PROD"]["DB_URI"])
-db = client.get_database("sample_mflix")
-comments = db.get_collection("comments")
+db = client.get_database("prod")
+users = db.get_collection("users")
+users.create_index("username", unique=True)
+ping = db.get_collection("ping")
+heatmaps = db.get_collection("heatmaps")
 
 
 # Test the database connection:
 def test_db_connection():
     try:
-        test_person = comments.find_one({"name": "Mercedes Tyler"})
-        if test_person:
-            return "The database is successfully connected"
-        # return "The database is successfully connected"
+        ping.insert_one({"ping": "1"})
+        ping.delete_one({"ping": "1"})
+        return "Pong. The database is successfully connected."
     except Exception as e:
         return f"Error connecting to the database: {e}"
+
+def addUser(user):
+    try:
+        return users.insert_one(user).inserted_id
+    except DuplicateKeyError:
+        return {"error": "User already exists."}
+    
+def authenticate(user):
+    username = user["username"]
+    password = user["password"]
+    try:
+        result = users.find_one({"username": username})
+        if result:
+            # check if the passwords and username matches up
+            if result["username"] == username and result["password"] == password:
+                return 0
+            else:
+                return 1
+        else:
+            return 2
+    except Exception as e:
+        raise Exception(f"An error occurred: {e}")
+
+def addHeatMap(request):
+    try:
+        username = request["username"]
+        habitName = request["habitName"]
+        habitData = request["habitData"]
+        heatmaps.find_one_and_update(
+            {"username": username},
+            {"$push": {f"habits.{habitName}.data": habitData}},
+            upsert=True
+        )
+    except Exception as e:
+        raise Exception(f"An error occurred: {e}")
+    
+def removeHeatMap(request):
+    try:
+        username = request["username"]
+        habitName = request["habitName"]
+        habitData = request["habitData"]
+        heatmaps.find_one_and_update(
+            {"username": username},
+            {"$pull": {f"habits.{habitName}.data": habitData}}
+        )
+    except Exception as e:
+        raise Exception(f"An error occurred: {e}")
+    
+def getHabits(username):
+    try:
+        result = heatmaps.find_one({"username": username})
+        if result is None:
+            return {"error": "User not found."}
+        return {"habits": result["habits"]}
+    except Exception as e:
+        raise Exception(f"An error occurred: {e}")
