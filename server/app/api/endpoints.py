2a247245242eb03dffc195f0185543ba32b56a28
Abrar Habib <abrarhabib285@gmail.com>
2024-05-20 14:30:55 -0400

    Successfully integrated json web token into project

--- server/app/api/endpoints.py
+++ server/app/api/endpoints.py
@@ -1,11 +1,12 @@
-from typing import Union
-from flask import Blueprint, request, jsonify
-from bson import ObjectId
+from flask import Blueprint, config, jsonify, request
+import bcrypt
 import jwt
 from datetime import datetime, timedelta
-from app.factory import bcrypt, secret
-import json
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+from datetime import datetime
 from app.db import (
+    config,
     addHeatMap,
     find_user_by_email,
     find_users,
@@ -18,20 +19,23 @@ from app.db import (
     removeHabit,
     updateHabit,
     updateMetric,
-    updateUsername
+    updateUsername,
 )
 
-from flask_cors import CORS
-# from app.api.utils import expect
-from datetime import datetime
+secret = config["JWT"]["SECRET"]
+print(secret)
+salt = bcrypt.gensalt()
+
 
 api_v1 = Blueprint("api_v1", "api_v1", url_prefix="/api/v1")
 
 CORS(api_v1)
 
+
 @api_v1.route("/ping", methods=["GET"])
 def test_db():
     return jsonify(test_db_connection())
+
 
 @api_v1.route("/register", methods=["POST"])
 def save_user():
@@ -41,11 +45,14 @@ def save_user():
     try:
         data = request.get_json()
         check = find_users(data)
-        if check.count() >=1 :
+        if check >= 1:
             message = "User with that email already exists."
             code = 400
         else:
-            data["password"] = bcrypt.generate_password_hash(data["password"]).decode("utf-8")
+            salt = bcrypt.gensalt()
+            data["password"] = bcrypt.hashpw(
+                data["password"].encode("utf-8"), salt
+            ).decode("utf-8")
             data["created_at"] = datetime.now()
             res = register(data)
             if res.acknowledged:
@@ -64,25 +71,30 @@ def login():
     code = 500
     status = "fail"
     res_data = {}
-    print(secret)
     try:
         data = request.get_json()
         user = find_user_by_email(data["email"])
         if user:
             user["_id"] = str(user["_id"])
-            if user and bcrypt.check_password_hash(user["password"], data["password"]):
-                token = jwt.encode({
+            if bcrypt.checkpw(
+                data["password"].encode("utf-8"), user["password"].encode("utf-8")
+            ):
+                token = jwt.encode(
+                    {
                         "user": {
-                            "email": f"{user['email']}",
-                            "id": f"{user['_id']}",
+                            "email": user["email"],
+                            "id": user["_id"],
                         },
-                        "exp": datetime.now() + timedelta(hours=24)
-                    }, secret)
+                        "exp": datetime.now() + timedelta(hours=24),
+                    },
+                    secret,
+                    algorithm="HS256",
+                )
                 del user["password"]
                 message = "User logged in successfully."
                 code = 200
                 status = "success"
-                res_data["token"] = token.decode("utf-8")
+                res_data["token"] = token
                 res_data["user"] = user
             else:
                 message = "Invalid credentials."
@@ -94,42 +106,8 @@ def login():
         message = str(e)
         status = "fail"
     return jsonify({"status": status, "message": message, "data": res_data}), code
-    
-@api_v1.route("/addMetric", methods=["POST"])
-def add_metric():
-    if not request.is_json:
-        return jsonify({"msg": "Not JSON request."}), 400
-    req = request.get_json()
-    try:
-        # Add the map to the database
-        addHeatMap(req)
-        return jsonify({"msg": "Data added to Habit successfully."}), 200
-    except Exception as e:
-        return jsonify({"msg": str(e)}), 400
-    
-@api_v1.route("/createHabit", methods=["POST"])
-def add_habit():
-    if not request.is_json:
-        return jsonify({"msg": "Not JSON request."}), 400
-    req = request.get_json()
-    try:
-        createHeatmap(req)
-        return jsonify({"msg": "Habit created successfully."}), 200
-    except Exception as e:
-        return jsonify({"msg": str(e)}), 400    
 
-@api_v1.route("/removeMetric", methods=["POST"])
-def remove_metric():
-    if not request.is_json:
-        return jsonify({"msg": "Not JSON request."}), 400
-    req = request.get_json()
-    try:
-        # Remove the metric from the database
-        removeMetric(req)
-        return jsonify({"msg": "Metric removed successfully."}), 200
-    except Exception as e:
-        return jsonify({"msg": str(e)}), 400
-    
+
 @api_v1.route("/removeHabit", methods=["POST"])
 def remove_habit():
     if not request.is_json:
@@ -141,7 +119,8 @@ def remove_habit():
         return jsonify({"msg": "habit removed successfully."}), 200
     except Exception as e:
         return jsonify({"msg": str(e)}), 400
-    
+
+
 @api_v1.route("/getHabits", methods=["POST"])
 def get_habits():
     if not request.is_json:
@@ -150,10 +129,11 @@ def get_habits():
     try:
         # Get the habits from the database
         habits = getHabits(req.get("username"))
-        return jsonify({"msg": "Habits retrieved successfully.", "result":habits}), 200
+        return jsonify({"msg": "Habits retrieved successfully.", "result": habits}), 200
     except Exception as e:
         return jsonify({"msg": str(e)}), 400
-    
+
+
 @api_v1.route("/updateHabit", methods=["POST"])
 def update_habit():
     if not request.is_json:
@@ -165,7 +145,8 @@ def update_habit():
         return jsonify({"msg": "Habit updated successfully."}), 200
     except Exception as e:
         return jsonify({"msg": str(e)}), 400
-    
+
+
 @api_v1.route("/updateMetric", methods=["POST"])
 def update_metric():
     if not request.is_json:
@@ -177,7 +158,8 @@ def update_metric():
         return jsonify({"msg": "Metric updated successfully."}), 200
     except Exception as e:
         return jsonify({"msg": str(e)}), 400
-    
+
+
 @api_v1.route("/updateUsername", methods=["POST"])
 def update_username():
     if not request.is_json:
