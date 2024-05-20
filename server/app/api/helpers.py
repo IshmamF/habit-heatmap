fb908ac2af1415cd4e2cd0d6ce946b133bfb3479
Abrar Habib <abrarhabib285@gmail.com>
2024-05-19 23:26:11 -0400

    Added a tokenreq helper for jwt validation

---
+++ server/app/api/helpers.py
@@ -0,0 +1,18 @@
+from functools import wraps
+from flask import request, jsonify
+import jwt
+from app.factory import secret
+
+def tokenReq(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if "Authorization" in request.headers:
+            token = request.headers["Authorization"]
+            try:
+                jwt.decode(token, secret)
+            except:
+                return jsonify({"status": "fail", "message": "unauthorized"}), 401
+            return f(*args, **kwargs)
+        else:
+            return jsonify({"status": "fail", "message": "unauthorized"}), 401
+    return decorated
