8ee7a86ade7bfc73bd3080a4a537696170c18eab
Yared Pena <102680810+YaredPena@users.noreply.github.com>
2024-05-21 02:37:45 -0400

    update navbar

--- controller/src/components/Navbar/Navbar.jsx
+++ controller/src/components/Navbar/Navbar.jsx
@@ -4,26 +4,33 @@ import logoDark from '../../assets/logo-dark.png';
 import toggleLight from '../../assets/night.png';
 import toggleDark from '../../assets/day.png';
 import { Link } from "react-router-dom";
+import { isAuthenticated } from '../../functions/auth';
 
 const Navbar = ({ theme, toggleTheme }) => {
-    return (
-        <nav className={`p-16 flex justify-between items-center transition-colors duration-500 ${theme === 'light' ? 'bg-gray-300 text-black' : 'bg-black text-white'}`}>
-            <div>
-                <img src={theme === 'light' ? logoLight : logoDark} alt="" className="h-auto w-40" />
-            </div>
-            <ul className="flex space-x-4">
-                <li><Link to="/" className="navbar-link transition-colors ">Home</Link></li>
-                <li><Link to="/add-habit" className="navbar-link transition-colors">Add Habit</Link></li>
-                <li><Link to="/habits" className="navbar-link transition-colors">Habits</Link></li>
-                <li><Link to="/settings" className="navbar-link transition-colors">Settings</Link></li>
-                <li><Link to="/about" className="navbar-link transition-colors">About</Link></li>
-            </ul>
-            <div>
-                {/* Invoke toggleTheme when the button is clicked */}
-                <img onClick={toggleTheme} src={theme === 'light' ? toggleLight : toggleDark} alt="" className="h-8" />
-            </div>
-        </nav>
-    );
-};
+    if (isAuthenticated()) {
+        return null; // this won't allow the navbar to render if the person is not authenticated.
+    }
+        return (
+            <nav className={`p-16 flex justify-between items-center transition-colors duration-500 ${theme === 'light' ? 'bg-gray-300 text-black' : 'bg-black text-white'}`}>
+                <div>
+                    <img src={theme === 'light' ? logoLight : logoDark} alt="" className="h-auto w-40" />
+                </div>
+                <ul className="flex space-x-4">
+                    <li><Link to="/" className="navbar-link transition-colors ">Home</Link></li>
+                    <li><Link to="/add-habit" className="navbar-link transition-colors">Add Habit</Link></li>
+                    <li><Link to="/habits" className="navbar-link transition-colors">Habits</Link></li>
+                    <li><Link to="/settings" className="navbar-link transition-colors">Settings</Link></li>
+                    <li><Link to="/about" className="navbar-link transition-colors">About</Link></li>
+                </ul>
+                <div>
+                    <Link to="/login" className="navbar-link transition-colors">Login</Link>
+                </div>
+                <div>
+                    {/* Invoke toggleTheme when the button is clicked */}
+                    <img onClick={toggleTheme} src={theme === 'light' ? toggleLight : toggleDark} alt="" className="h-8" />
+                </div>
+            </nav>
+        );
+    };
 
 export default Navbar;
