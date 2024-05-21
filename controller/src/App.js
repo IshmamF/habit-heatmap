0c07aa2886d4c2ecfc4e70b338ed71788dfd22ea
Yared Pena <102680810+YaredPena@users.noreply.github.com>
2024-05-21 02:39:08 -0400

    updated App.js

--- controller/src/App.js
+++ controller/src/App.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import AddHabit from './components/Pages/AddHabit';
 import Habit from './components/Pages/Habit';
 import Home from './components/Pages/Home';
 import About from './components/Pages/About';
 import Settings from './components/Pages/Settings';
+import LoginPage from './components/Pages/Login';
+import SignupPage from './components/Pages/Signup'; 
+import { isAuthenticated } from './functions/auth';
 
 const App = () => {
   const currentTheme = localStorage.getItem('current_theme');
@@ -24,20 +27,20 @@ const App = () => {
   return (
     <div className={`transition-colors duration-500 ease-in-out h-screen ${theme === 'light' ? 'bg-gray-100 text-black' : 'bg-zinc-900 text-white'}`}>
       <Navbar theme={theme} toggleTheme={toggleTheme} />
-
       <div className={`container mx-auto px-4 ${theme}`}>
-        {/* react router routes */}
         <Routes>
-          <Route path="/" element={<Home username={username} />} />
-          <Route path="/about" element={<About username={username} />} />
-          <Route path="/settings" element={<Settings username={username} setUsername={setUsername}/>} />
-          <Route path="/habits" element={<Habit username={username}/>} />
-          <Route path="/add-habit" element={<AddHabit username={username}/>} />
+          <Route path="/" element={isAuthenticated() ? <Home username={username} /> : <Navigate to="/login" />} />
+          <Route path="/about" element={isAuthenticated() ? <About username={username} /> : <Navigate to="/login" />} />
+          <Route path="/settings" element={isAuthenticated() ? <Settings username={username} setUsername={setUsername} /> : <Navigate to="/login" />} />
+          <Route path="/habits" element={isAuthenticated() ? <Habit username={username} /> : <Navigate to="/login" />} />
+          <Route path="/add-habit" element={isAuthenticated() ? <AddHabit username={username} /> : <Navigate to="/login" />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
-
       </div>
     </div>
   );
-}
+};
 
 export default App;
