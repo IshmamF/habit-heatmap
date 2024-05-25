2211a6b9628daf08d650714e076afd8922374e28
Yared Pena <102680810+YaredPena@users.noreply.github.com>
2024-05-25 01:24:37 -0400

    Update About Page

--- controller/src/components/Pages/About.jsx
+++ controller/src/components/Pages/About.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 const About = ({ theme }) => {
   const [text, setText] = useState('');
-  const bioText = "Habit Heatmap is a goal tracker that will allow users to mark their goals and activities through the use of heatmaps. This project was created by Ishmam Fardin, Abrar Habib, and Yared Pena";
+  const bioText = "Habit Heatmap is a goal tracker that will allow users to mark their goals and activities through the use of heatmaps. This project was created by Ishmam Fardin, Abrar Habib, and Yared Pena.";
 
   useEffect(() => {
     let index = 0;
@@ -21,7 +21,7 @@ const About = ({ theme }) => {
     return () => {
       clearTimeout(typingTimeout);
     };
-  }, [bioText]);
+  }, []);
 
   return (
     <div className={`flex items-center justify-center min-h-screen transition-all duration-500 ease-in-out ${theme === 'light' ? 'bg-gray-100 text-black' : 'bg-zinc-900 text-white'}`}>
